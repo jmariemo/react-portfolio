4e9303905065bac7d3ed95faa72a2c9449604ba4
Jennifer Rosado <Jennifer@Jennifer.local>
2023-09-25 11:07:21 -0700

    initialize react app, configure tailwind

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <h1 className='text-6xl text-rosado'>Hello Tailwind</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        rosado: "rgb(244, 223, 213)",
+      },
+    },
+  },
+  plugins: [],
+};
