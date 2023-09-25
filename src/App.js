36e33c1a25b9e78e82b5b95165da8cda077d9d95
Jennifer Rosado <Jennifer@Jennifer.local>
2023-09-25 15:04:50 -0700

    add components with styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import './App.css';
+import React, { useState } from "react";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("About");
+
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
-    <div className="App">
-      <div>
-        <h1 className='text-6xl text-rosado'>Hello Tailwind</h1>
-      </div>
+    <div>
+      <Header
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+      {renderPage()}
+      <Footer />
     </div>
   );
 }
